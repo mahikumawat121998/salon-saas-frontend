71ac15db0768dbf174da0a297a01a74752070d58
mahesh-kumawat-9x-dev <mahesh.k@9xtechnology.com>
2026-09-14 15:55:53 +0530

    fix: remove invalid generateStaticParams to fix next-on-pages build 500 crash

--- src/app/(tenant)/dashboard/staff/[id]/page.tsx
+++ src/app/(tenant)/dashboard/staff/[id]/page.tsx
@@ -1,8 +1,5 @@
 import DetailedStaffProfileClient from './StaffDetailClient';
 
-export function generateStaticParams() {
-  return [{ id: '1' }];
-}
 
 export default function DetailedStaffProfilePage() {
   return <DetailedStaffProfileClient />;
